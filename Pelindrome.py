934505b7508d3e6d9806cadfba143a110ac344d9
uday928 <udaygandhi2811@gmail.com>
2024-04-11 08:17:20 +0530

    ppfd

---
+++ Pelindrome.py
@@ -0,0 +1,16 @@
+# by using start stop step 
+str1=input()
+if str1[::1]==str1[::-1]:
+    print("pelindrome")
+else:
+    print("not pelindrome")
+
+# # by using reversed string
+# str1=input()
+# str2=""
+# for i in str1:
+#     str2=i+str2 
+# if str1==str2:
+#     print("pelindrome")
+# else:
+#     print("not pelindrome")
